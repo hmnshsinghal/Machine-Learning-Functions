72a0922fafa4934ba5c32b7e47959121c239c066
Himanshu Singhal <65439220+hmnshsinghal@users.noreply.github.com>
2021-12-16 16:14:12 +0530

    Update kfold_keras.py

--- kfold_keras.py
+++ kfold_keras.py
@@ -1,8 +1,9 @@
-def keras_kfold_validation(X,y,K_count):
+def keras_kfold_validation(X,y,K_count,input_dims):
     from sklearn.model_selection import KFold
     import tensorflow.keras
     from keras.models import Sequential
     from keras.layers import Dense
+    from sklearn.metrics import classification_report,confusion_matrix,accuracy_score,precision_recall_curve,recall_score,precision_score
     kfold_validation=KFold(K_count)
     Fold=0
     train_dict={}
@@ -21,12 +22,12 @@ def keras_kfold_validation(X,y,K_count):
         train_dict[Fold]=[X_train_kf,y_train_kf]
         test_dict[Fold]=[X_test_kf,y_test_kf]
         model = Sequential()
-        model.add(Dense(150, input_dim=6, activation='relu'))
+        model.add(Dense(150, input_dim=input_dims, activation='relu'))
         model.add(Dense(150, activation='softmax'))
         model.add(Dense(150, activation='linear'))
         model.add(Dense(1, activation='linear'))
         model.compile(optimizer='adam', loss='mse', metrics=['mse'])
-        epochs_hist = model.fit(np.array(X_train), y_train, epochs=250, batch_size=25,  verbose=1, validation_split=0.4)
+        epochs_hist = model.fit(np.array(X_train), y_train, epochs=250, batch_size=25,  verbose=1)
         y_predict_kf = np.round(model.predict(np.array(X_test_kf)))
         classification_dict[Fold]=[y_test_kf,y_predict_kf]
         accuracy_dict[Fold]=accuracy_score(y_test_kf,y_predict_kf)
